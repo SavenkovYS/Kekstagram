909df2eb4b676a71426eff7d9f68df121ffb14d4
Юрий Савенков <yura-savenkov1@yandex.ru>
2021-01-08 20:44:25 +0300

    hashtag filter

--- docs/js/pictures.js
+++ docs/js/pictures.js
@@ -129,3 +129,43 @@ function handleFileUpload() {
     });
 }
 
+
+const hashtagsInput = document.querySelector('.text__hashtags');
+const uploadForm = document.getElementById('upload-select-image');
+
+let userHashtags = []
+
+uploadForm.addEventListener('submit', handleHashtagValidation);
+
+hashtagsInput.addEventListener('input', handleHashtagInput)
+
+function handleHashtagInput() {
+    userHashtags = hashtagsInput.value.trim().split(' ').filter(item => item !== '');  
+    
+    const lowerCaseHashtags = userHashtags.map(hashtag => hashtag.toLowerCase());
+
+    console.log(lowerCaseHashtags);
+
+    lowerCaseHashtags.forEach((hashtag, i, initArray) => {
+
+        if (hashtag[0] !== '#') {
+            hashtagsInput.setCustomValidity('Хэштэг должен начинаться с #');
+        } else if (hashtag === '#') {
+            hashtagsInput.setCustomValidity('Хэштэг не может состоять только из символа #');
+        } else if (hashtag.length > 20) {
+            hashtagsInput.setCustomValidity('Максимальная длина хэштэга 20 символов');
+        } else if (initArray.indexOf(hashtag) !== i) {
+            hashtagsInput.setCustomValidity('Один хэштэг нельзя использовать дважды');
+        } else if (initArray.length > 5) {
+            hashtagsInput.setCustomValidity('Максимум можно поставить 5 хэштэгов');
+        } else {
+            hashtagsInput.setCustomValidity('');
+        }
+    })
+}
+
+function handleHashtagValidation(event) {
+    event.preventDefault();
+
+    
+}
