a69bed1a133f1f137b28d0aff6d6314f604961f4
Юрий Савенков <yura-savenkov1@yandex.ru>
2021-01-13 17:50:37 +0300

    filters

--- docs/js/pictures.js
+++ docs/js/pictures.js
@@ -189,9 +189,13 @@ const filterBtns = document.querySelectorAll('.effects__radio');
 const filterRangeContainer = document.querySelector('.img-upload__scale');
 const filterRange = filterRangeContainer.querySelector('.scale__value');
 const filterRangePin = filterRangeContainer.querySelector('.scale__pin');
+const filterRangeLine = filterRangeContainer.querySelector('.scale__line');
+const filterRangeLevel = filterRangeContainer.querySelector('.scale__level');
 
 filterBtns.forEach(btn => {
     btn.addEventListener('click', () => {
+        filterRange.value = 100;
+        setFilters();
         let value = null;
         for (let i = 0; i < filterBtns.length; i++) {
             value = filterBtns[i].value;
@@ -206,38 +210,80 @@ filterBtns.forEach(btn => {
             filterRangeContainer.classList.add('hidden');
         } else {
             filterRangeContainer.classList.remove('hidden');
+            filterRangePin.style.left = `${filterRangeLevel.offsetWidth}px`;   
         }
     })
 })
 
-let x = null;
-let y = null;
-
 filterRangePin.addEventListener('mousedown', event => {
+    event.preventDefault();
 
-    filterRangePin.style.position = 'absolute';
-    
-    moveAt(event);
+    const filterLevelCoords = filterRangeLevel.getBoundingClientRect();
+    const filterCoords = {};
+    filterCoords.top = filterLevelCoords.top + pageYOffset;
+    filterCoords.left = filterLevelCoords.left + pageXOffset;
 
-    filterRangePin.style.zIndex = 1000;
+    let newLeftPosition = null;
+    const filterRangePinClientCoords = filterRangePin.getBoundingClientRect();
+    const filterRangePinCoords = {};
+    filterRangePinCoords.top = filterRangePinClientCoords.top + pageYOffset;
+    filterRangePinCoords.left = filterRangePinClientCoords.left + pageXOffset;
+
+    let right = filterRangeLine.offsetWidth;
 
     document.addEventListener('mousemove', handleMouseMove)
 
-    document.addEventListener('mouseup', handleMouseUp)
-    // userImg.style.filter = `grayscale(${parseInt(filterRange.value) / 100})`
+    function handleMouseMove(event) {
+        event.preventDefault();
+
+        newLeftPosition = event.pageX - filterCoords.left;
+        if (newLeftPosition < 0) {
+            newLeftPosition = 0;
+        }
+        if (newLeftPosition > right) {
+            newLeftPosition = right;
+        }
+        filterRangePin.style.left = `${newLeftPosition}px`;
+
+        filterRange.value = Math.floor(newLeftPosition / filterRangeLine.offsetWidth * 100);
+        setFilters();
+    
+        document.addEventListener('mouseup', handleMouseUp)
+    }
+    
+    function handleMouseUp() {
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+        
+        setFilters();
+    }
 })
 
-function moveAt(e) {
-    filterRangePin.style.left = `${e.pageX - filterRangePin.getBoundingClientRect().x}px`
-    console.log(filterRangePin.getBoundingClientRect())
-    console.log(e.pageX)  
+function setFilters() {
+    const value = filterRange.value; 
+    for (let i = 0; i < filterBtns.length; i++) {
+        if (filterBtns[i].checked) {
+            switch(filterBtns[i].value) {
+                case 'none':
+                    userImg.style.filter = null;
+                    break;
+                case 'chrome':
+                    userImg.style.filter = `grayscale(${value / 100})`;
+                    break;
+                case 'sepia':
+                    userImg.style.filter = `sepia(${value / 100})`;
+                    break;
+                case 'marvin':
+                    userImg.style.filter = `invert(${value}%)`;
+                    break;
+                case 'phobos':
+                    userImg.style.filter = `blur(${value / 100 * 3}px)`;
+                    break;
+                case 'heat':
+                    userImg.style.filter = `brightness(${1 + value / 100 * 2})`;
+                    break;
+            }
+        }
+    } 
 }
 
-function handleMouseMove(event) {
-    moveAt(event);    
-}
-
-function handleMouseUp() {
-    document.removeEventListener('mousemove', handleMouseMove);
-    document.removeEventListener('mouseup', handleMouseUp);
-}
