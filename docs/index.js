ccc7b7d405fece7225ed7cc239dc97d96d247f2a
Юрий Савенков <yura-savenkov1@yandex.ru>
2021-01-19 12:40:59 +0300

    validation

--- docs/index.js
+++ docs/index.js
@@ -2,3 +2,4 @@ import getPictures from './js/gallery.js';
 import handleFileUpload from './js/userPictureUpload.js';
 import scaleUserPicture from './js/scale.js';
 import filterUserPicture from './js/filter.js';
+import setValidity from './js/validation.js';
--- docs/js/validation.js
+++ docs/js/validation.js
@@ -1,31 +1,43 @@
-const hashtagsInput = document.querySelector('.text__hashtags');
-const uploadForm = document.getElementById('upload-select-image');
+function setValidity() {
+    const hashtagsInput = document.querySelector('.text__hashtags');
+    const commentInput = document.querySelector('.text__description');
 
-let userHashtags = []
+    const userHashtags = [];
+    const userComment = '';
 
-hashtagsInput.addEventListener('input', handleHashtagInput)
-
-function handleHashtagInput() {
-    userHashtags = hashtagsInput.value.trim().split(' ').filter(item => item !== '');  
+    hashtagsInput.addEventListener('input', onHashtagInput);
+    commentInput.addEventListener('input', onCommentInput);
     
-    const lowerCaseHashtags = userHashtags.map(hashtag => hashtag.toLowerCase());
+    function onHashtagInput() {
+        userHashtags = hashtagsInput.value.trim().split(' ').filter(item => item !== '');  
+        const lowerCaseHashtags = userHashtags.map(hashtag => hashtag.toLowerCase());
 
-    console.log(lowerCaseHashtags);
+        lowerCaseHashtags.forEach((hashtag, i, initArray) => {
+            if (hashtag[0] !== '#') {
+                hashtagsInput.setCustomValidity('Хэштэг должен начинаться с #');
+            } else if (hashtag === '#') {
+                hashtagsInput.setCustomValidity('Хэштэг не может состоять только из символа #');
+            } else if (hashtag.length > 20) {
+                hashtagsInput.setCustomValidity('Максимальная длина хэштэга 20 символов');
+            } else if (initArray.indexOf(hashtag) !== i) {
+                hashtagsInput.setCustomValidity('Один хэштэг нельзя использовать дважды');
+            } else if (initArray.length > 5) {
+                hashtagsInput.setCustomValidity('Максимум можно поставить 5 хэштэгов');
+            } else {
+                hashtagsInput.setCustomValidity('');
+            }
+        })
+    }
 
-    lowerCaseHashtags.forEach((hashtag, i, initArray) => {
-
-        if (hashtag[0] !== '#') {
-            hashtagsInput.setCustomValidity('Хэштэг должен начинаться с #');
-        } else if (hashtag === '#') {
-            hashtagsInput.setCustomValidity('Хэштэг не может состоять только из символа #');
-        } else if (hashtag.length > 20) {
-            hashtagsInput.setCustomValidity('Максимальная длина хэштэга 20 символов');
-        } else if (initArray.indexOf(hashtag) !== i) {
-            hashtagsInput.setCustomValidity('Один хэштэг нельзя использовать дважды');
-        } else if (initArray.length > 5) {
-            hashtagsInput.setCustomValidity('Максимум можно поставить 5 хэштэгов');
-        } else {
-            hashtagsInput.setCustomValidity('');
+    function onCommentInput() {
+        if (userComment.length > 140) {
+            commentInput.setCustomValidity('Длина комментария не должна превышать 140 символов');
         }
-    })
+    }
 }
+
+setValidity();
+
+export default setValidity;
+
+
