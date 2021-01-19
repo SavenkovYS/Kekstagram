8ede81881433717e8473895523f122274dd4ef20
Юрий Савенков <yura-savenkov1@yandex.ru>
2021-01-19 12:20:21 +0300

    filters

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,4 @@
 import getPictures from './js/gallery.js';
 import handleFileUpload from './js/userPictureUpload.js';
 import scaleUserPicture from './js/scale.js';
+import filterUserPicture from './js/filter.js';
---
+++ docs/js/filter.js
@@ -0,0 +1,105 @@
+function filterUserPicture() {
+    const userImg = document.getElementById('user-picture');
+    const filterBtns = document.querySelectorAll('.effects__radio');
+    const sliderContainer = document.querySelector('.img-upload__scale');
+    const sliderInput = sliderContainer.querySelector('.scale__value');
+    const slider = sliderContainer.querySelector('.scale__line');
+    const sliderPin = sliderContainer.querySelector('.scale__pin');  
+    const sliderLevel = sliderContainer.querySelector('.scale__level');
+
+    filterBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            sliderInput.value = 100;
+            setFilters();
+            let classValue = null;
+            for (let i = 0; i < filterBtns.length; i++) {
+                classValue = filterBtns[i].value;
+                if (filterBtns[i].checked) {
+                    userImg.classList.add(`effects__preview--${classValue}`);
+                } else {
+                    userImg.classList.remove(`effects__preview--${classValue}`);
+                }
+            }
+            
+            if(btn.value === 'none') {
+                sliderContainer.classList.add('hidden');
+            } else {
+                sliderContainer.classList.remove('hidden');
+                sliderPin.style.left = `${sliderLevel.offsetWidth}px`;   
+            }
+        })
+    })
+
+    sliderPin.addEventListener('mousedown', event => {
+        const sliderLevelDefaultCoords = sliderLevel.getBoundingClientRect();
+        const sliderLevelCoords = {};
+        sliderLevelCoords.top = sliderLevelDefaultCoords.top + pageYOffset;
+        sliderLevelCoords.left = sliderLevelDefaultCoords.left + pageXOffset;
+
+        
+        const sliderPinDefaultCoords = sliderPin.getBoundingClientRect();
+        const sliderPinCoords = {};
+        sliderPinCoords.top = sliderPinDefaultCoords.top + pageYOffset;
+        sliderPinCoords.left = sliderPinDefaultCoords.left + pageXOffset;
+
+        let newLeftPosition = null;
+        let right = sliderLevel.offsetWidth; //
+
+        document.addEventListener('mousemove', onMouseMove)
+
+        function onMouseMove(event) {
+            newLeftPosition = event.pageX - sliderLevelCoords.left;
+            if (newLeftPosition < 0) {
+                newLeftPosition = 0;
+            }
+            if (newLeftPosition > right) {
+                newLeftPosition = right;
+            }
+            sliderPin.style.left = `${newLeftPosition}px`;
+
+            sliderInput.value = Math.floor(newLeftPosition / sliderLevel.offsetWidth * 100); //
+            setFilters();
+        
+            document.addEventListener('mouseup', onMouseUp)
+        }
+        
+        function onMouseUp() {
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseup', onMouseUp);
+            
+            setFilters();
+        }
+    })
+
+    function setFilters() {
+        const value = sliderInput.value; 
+        for (let i = 0; i < filterBtns.length; i++) {
+            if (filterBtns[i].checked) {
+                switch(filterBtns[i].value) {
+                    case 'none':
+                        userImg.style.filter = null;
+                        break;
+                    case 'chrome':
+                        userImg.style.filter = `grayscale(${value / 100})`;
+                        break;
+                    case 'sepia':
+                        userImg.style.filter = `sepia(${value / 100})`;
+                        break;
+                    case 'marvin':
+                        userImg.style.filter = `invert(${value}%)`;
+                        break;
+                    case 'phobos':
+                        userImg.style.filter = `blur(${value / 100 * 3}px)`;
+                        break;
+                    case 'heat':
+                        userImg.style.filter = `brightness(${1 + value / 100 * 2})`;
+                        break;
+                }
+            }
+        } 
+    }
+}
+
+filterUserPicture();
+
+export default filterUserPicture;
